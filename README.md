6044f89b45f96d2f1a688c05d1dd0f6f55f53079
Jackie Deng <jackiedeng@proton.me>
2025-01-21 22:15:58 +1100

    play music + frequency bars

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ebiten Music
+## Math
+Ebitengine ticks per second (TPS) = 60.
+Time per tick = 1/60 s = 0.0167.
+Typical sampling frequency = 48000.
+Samples per tick = 0.0167 * 48000 = 800.
+32 bit left + 32 bit right = 8 bytes.
--- main.go
+++ main.go
@@ -1,45 +1,113 @@
 package main
 
 import (
+	"encoding/binary"
 	"image/color"
+	"io"
 	"math"
 
+	"math/cmplx"
+	"os"
+	"time"
+
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/mp3"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"gonum.org/v1/gonum/dsp/fourier"
 )
 
 type MusicBarsSettings struct {
 	NumBars   uint
 	MaxWidth  float64
 	MaxHeight float64
-	StartFreq float64
-	EndFreq   float64
 }
 
-type Bar struct {
-	Freq      float64
-	HeightPct float64
-}
-
-func (settings *MusicBarsSettings) initBars() []Bar {
-	bars := make([]Bar, settings.NumBars)
+func (settings *MusicBarsSettings) initBars() []float64 {
+	bars := make([]float64, settings.NumBars)
 	for i := range settings.NumBars {
-		freq := (settings.StartFreq * math.Pow(
-			settings.EndFreq/settings.StartFreq, float64(i/settings.NumBars)))
-		// TODO: set initial HeightPct to 0
-		bars[i] = Bar{Freq: freq, HeightPct: (float64(i) * 10) / 100}
+		bars[i] = 0
 	}
 	return bars
 }
 
 var settings MusicBarsSettings
-var bars []Bar
+var bars []float64
 var barWidth float64
+var visualizerStream *mp3.Stream
+
+var startTime time.Time
 
 type Game struct{}
 
+func invariant(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type Stereo struct {
+	Left  float32
+	Right float32
+}
+
+var stereo []Stereo
+var fft *fourier.FFT
+var leftF64 []float64
+
+const (
+	BytesPerSample     = 8
+	TicksPerSecond     = 60
+	Bars               = 20
+	MaxLogAmplitude    = 4.0
+	MinLogAmplitude    = -2.0
+	MovingAverageRatio = 0.3
+)
+
+var frequenciesPerBar int
+
+func VisualizerSetup() {
+	var samplesPerTick int = visualizerStream.SampleRate() / TicksPerSecond
+	stereo = make([]Stereo, samplesPerTick)
+	fft = fourier.NewFFT(samplesPerTick)
+	leftF64 = make([]float64, samplesPerTick)
+	fftFrequencies := fft.Len()/2 + 1
+	frequenciesPerBar = fftFrequencies / Bars
+}
+
 func (g *Game) Update() error {
-	return nil
+	visualizerStream.Seek(int64(float64(time.Since(startTime).Milliseconds())*
+		float64(visualizerStream.SampleRate())/1000.0)*BytesPerSample,
+		io.SeekStart)
+
+	err := binary.Read(visualizerStream, binary.LittleEndian, &stereo)
+	for i, v := range stereo {
+		leftF64[i] = float64(v.Left)
+	}
+	coeff := fft.Coefficients(nil, leftF64)
+	runningSum := 0.0
+	for i, c := range coeff[1:] {
+		runningSum += cmplx.Abs(c)
+		if i%frequenciesPerBar == 0 {
+			if i/frequenciesPerBar == Bars {
+				break
+			}
+			logAverageFrequency := math.Log(runningSum / float64(frequenciesPerBar))
+			currentCappedFrequency := 0.0
+			if logAverageFrequency > MaxLogAmplitude {
+				currentCappedFrequency = 1.0
+				bars[i/frequenciesPerBar] = 1.0
+			} else if logAverageFrequency < MinLogAmplitude {
+				currentCappedFrequency = 0.0
+				bars[i/frequenciesPerBar] = 0.0
+			} else {
+				currentCappedFrequency = ((logAverageFrequency - MinLogAmplitude) / (MaxLogAmplitude - MinLogAmplitude))
+			}
+			bars[i/frequenciesPerBar] = (bars[i/frequenciesPerBar]*MovingAverageRatio + currentCappedFrequency*(1-MovingAverageRatio))
+			runningSum = 0.0
+		}
+	}
+	return err
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -47,7 +115,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	for i, bar := range bars {
 		ebitenutil.DrawRect(screen,
 			float64(i)*barWidth, 240,
-			barWidth, -(bar.HeightPct * settings.MaxHeight),
+			barWidth, -(bar * settings.MaxHeight),
 			rectColor)
 	}
 }
@@ -57,12 +125,32 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		panic("Takes an mp3 file as argument.")
+	}
+	file, err := os.Open(os.Args[1])
+	defer file.Close()
+	invariant(err)
+	audioStream, err := mp3.DecodeF32(file)
+	invariant(err)
+
+	file2, err := os.Open(os.Args[1])
+	defer file2.Close()
+	invariant(err)
+	visualizerStream, err = mp3.DecodeF32(file2)
+	invariant(err)
+	VisualizerSetup()
+
+	context := audio.NewContext(audioStream.SampleRate())
+	musicPlayer, err := context.NewPlayerF32(audioStream)
+	invariant(err)
+	musicPlayer.Play()
+	startTime = time.Now()
+
 	settings = MusicBarsSettings{
-		NumBars:   10,
+		NumBars:   20,
 		MaxWidth:  320,
 		MaxHeight: 240,
-		StartFreq: 20,
-		EndFreq:   20,
 	}
 	bars = settings.initBars()
 	barWidth = settings.MaxWidth / float64(settings.NumBars)
